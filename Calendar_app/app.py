b4d5013f966b085a18933341856b956bbe151b42
takiyama-aki <57694434+takiyama-aki@users.noreply.github.com>
2022-03-07 21:17:07 +0900

    start
    オウム返しのチャットボットと、Googleカレンダーのクイックスタートファイル

---
+++ Calendar_app/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('lhqD0fJxzv4nMgoIOdqLeMrTdNTFFaEhI8vX/PBYSDMndxQNRhIVJcTTXM+0txH2IvUDVtHiZpIspqirjCS2mbx0nJ4Gsq4BeB2RIEVHjWTzk/JkXAzuIfiMgzUzmCOIWqoQj66UANRU3xeaYJM1PgdB04t89/1O/w1cDnyilFU=')
+#YOUR_CHANNEL_ACCESS_TOKEN
+handler = WebhookHandler('03fb497fc092b55b7ce856b2d9b795bd') 
+#YOUR_CHANNEL_SECRET
+
+@app.route("/")
+def test():
+    return "OK"
+    
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    receive_txt = event.message.text #"○月○日 △時△分 予定"
+    a = receive_txt.split(" ")
+    print(a)
+    reply_txt = "あなたは" + receive_txt + "と言った。"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_txt)) #event.message.text がユーザーが送ってきたテキスト
+    
+
+
+if __name__ == "__main__":
+    app.run()
